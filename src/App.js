066b07e2cf8b46f7ee5fe50a776956d178308f86
unknown <rahularora993676@gmail.com>
2023-01-10 23:28:02 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>About Us</h1>
+      <p>At Zappos.com, our purpose is simple: to live and deliver WOW.</p>
+      <p> Twenty years ago, we began as a small online retailer that only sold
+          shoes. Today, we still sell shoes — as well as <br /> clothing, handbags,
+          accessories, and more. That "more" is providing the very best customer
+          service, customer <br /> experience, and company culture. We aim to inspire
+          the world by showing it's possible to simultaneously deliver <br /> happiness
+          to customers, employees, vendors, shareholders, and the community in a
+          long-term, sustainable way.</p>
+      <p>We hope thta in the future people won't even realize we started br
+        selling shoes online . instead they will know zappes as a <br /> service company that just happens to sell --------</p>
+    </div>
+  );
+}
+
+export default App;
